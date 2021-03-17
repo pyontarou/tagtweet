a7bb82a0efa0f0f15983ba19d34a5e0a8453baa6
pyontarou <kooooo7150@gmail.com>
2021-03-17 23:05:28 +0900

    likeモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets 
-  has_many :comments      
+  has_many :comments 
+  has_many :likes, dependent: :destroy
+  has_many :like_tweets, through: :likes, source: :tweet    
+
+  def like(tweet)
+    likes.find_or_create_by(tweet: tweet)
+  end
+  
+  def like?(tweet)
+    like_tweets.include?(tweet)
+  end
+  
+  def unlike(tweet)
+    like_tweets.delete(tweet)
+  end
 end

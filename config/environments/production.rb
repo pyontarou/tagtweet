0b936c34535f0edd2d0e47a9ce3e12ba4a86114b
pyontarou <kooooo7150@gmail.com>
2021-03-17 16:42:28 +0900

    本番環境への設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://tagtweet.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://tagtweet.herokuapp.com', 'http://tagtweet.herokuapp.com']
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
